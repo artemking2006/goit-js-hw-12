ce2a85fc8ce4fa57a0a303ff8a4519f451d5d5db
Artem Zakharchuk <zakharchukartem18@gmail.com>
2025-02-20 16:06:02 +0200

    New changes!

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -18,64 +18,62 @@ const iziOption = {
   displayMode: 'replace',
   closeOnClick: true,
 };
-
 let page = 1;
 let perPage = 15;
 
 export function resetPage() {
-    page = 1;
+  page = 1;
 }
-
 export function addPage() {
-    page += 1;
+  page += 1;
 }
 
 function endOfList(daddyElement) {
-    removeLoadStroke(daddyElement);
-    daddyElement.insertAdjacentHTML(
-        'beforeend',
-        '<p class="loading-text">We\'re sorry, but you\'ve reached the end of search results.</p>'
-    );
-    addMoreButton.classList.add('hide');
+  removeLoadStroke(daddyElement);
+  daddyElement.insertAdjacentHTML(
+    'beforeend',
+    '<p class="loading-text">We\'re sorry, but you\'ve reached the end of search results .</p>'
+  );
+  addMoreButton.classList.add('hide');
 }
 
 export async function getImage(input) {
-    const API_KEY = '48883219-a8d6c51151168ef356226c6a1';
-    const query = encodeURIComponent(input);
-    const urlParams = new URLSearchParams({
-        key: API_KEY,
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true',
-        page: page,
-        per_page: perPage,
-    });
-    const URL = `https://pixabay.com/api/?${urlParams}`;
+  const API_KEY = '48883219-a8d6c51151168ef356226c6a1';
+  const query = encodeURIComponent(input);
+  const urlParams = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    page: page,
+    per_page: perPage,
+  });
+  const URL = `https://pixabay.com/api/?${urlParams}`;
 
-    try {
-        const { data } = await axios.get(URL);
-        markup(data);
-        if (data.totalHits < page * perPage) {
-            endOfList(load);
-            return;
-        }
-        if (page >= 2) {
-            const list = document.querySelector('.gallery__item');
-            const rect = list.getBoundingClientRect();
-            window.scrollBy({
-                top: rect.height * 2,
-                behavior: 'smooth',
-            });
-        }
-    } catch (error) {
-        console.error(error);
-        box.innerHTML = '';
-        load.innerHTML = '';
-        iziToast.show({
-            ...iziOption,
-            message: 'Sorry, an error happened. Try again',
-        });
-        return;
+  try {
+    const { data } = await axios.get(URL);
+    markup(data);
+    if (data.totalHits < page * perPage) {
+      endOfList(load);
+      return;
     }
+    if (page >= 2) {
+      const list = document.querySelector('.gallery__item');
+      const rect = list.getBoundingClientRect();
+      window.scrollBy({
+        top: rect.height * 2,
+        behavior: 'smooth',
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    box.innerHTML = '';
+    load.innerHTML = '';
+    iziToast.show({
+      ...iziOption,
+      message: 'Sorry, an error happened. Try again',
+    });
+    return;
+  }
 }
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -8,71 +8,73 @@ const box = document.querySelector('.gallery');
 const load = document.querySelector('.load');
 const addMoreButton = document.querySelector('.add-more-button');
 const iziOption = {
-    messageColor: '#FAFAFB',
-    messageSize: '16px',
-    backgroundColor: '#EF4040',
-    iconUrl: errorIcon,
-    transitionIn: 'bounceInLeft',
-    position: 'topRight',
-    displayMode: 'replace',
-    closeOnClick: true,
+  messageColor: '#FAFAFB',
+  messageSize: '16px',
+  backgroundColor: '#EF4040',
+  iconUrl: errorIcon,
+  transitionIn: 'bounceInLeft',
+  position: 'topRight',
+  displayMode: 'replace',
+  closeOnClick: true,
 };
 
 export function addLoadStroke(daddyElement) {
-    daddyElement.insertAdjacentHTML(
-        'beforeend',
-        '<p class="loading-text">Wait, the image is loaded</p><span class="loader"></span>'
-    );
-    addMoreButton.classList.add('hide');
+  daddyElement.insertAdjacentHTML(
+    'beforeend',
+    '<p class="loading-text">Wait, the image is loaded</p><span class="loader"></span>'
+  );
+  addMoreButton.classList.add('hide');
 }
 
 export function removeLoadStroke(daddyElement) {
-    const textElement = daddyElement.querySelector('.loading-text');
-    const loaderElement = daddyElement.querySelector('.loader');
+  const textElement = daddyElement.querySelector('.loading-text');
+  const loaderElement = daddyElement.querySelector('.loader');
 
-    if (textElement) textElement.remove();
-    if (loaderElement) loaderElement.remove();
+  if (textElement) textElement.remove();
+  if (loaderElement) loaderElement.remove();
 
-    addMoreButton.classList.remove('hide');
+  addMoreButton.classList.remove('hide');
 }
 
 export function markup(data) {
-    const { hits } = data;
-    if (hits.length === 0) {
-        iziToast.show({
-            ...iziOption,
-            message:
-            'Sorry, there are no images matching your search query. Please, try again!',
-        });
-        box.innerHTML = '';
-        return;
-    }
-    const markup = hits
-      .map(
-        image =>
-            `<li class='gallery__item'>
-           <a class='gallery__link' href="${image.largeImageURL}">
-           <img class='gallery__img' src="${image.webFormatURL}" alt="${image.tags}" />
-              <div class="grid">
-                <p>Likes</p>
-                <p>Views</p>
-                <p>Comments</p>
-                <p>Downloads</p>
-                <span>${image.likes}</span>
-                <span>${image.views}</span>
-                <span>${image.comments}</span>
-                <span>${image.downloads}</span>
-              </div>
-           </a>
-        </li>`
-      )
-      .join(' ');
-      removeLoadStroke(load);
-      box.insertAdjacentElement('beforeend', markup);
+  const { hits } = data;
 
-      const lightbox = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-      lightbox.refresh();
+  if (hits.length === 0) {
+    iziToast.show({
+      ...iziOption,
+      message:
+        'Sorry, there are no images matching your search query. Please, try again!',
+    });
+    box.innerHTML = '';
+
+    return;
+  }
+  const markup = hits
+    .map(
+      image =>
+        `<li class='gallery__item'>
+        <a class='gallery__link' href="${image.largeImageURL}">
+        <img class='gallery__img' src="${image.webformatURL}" alt="${image.tags}" />
+          <div class="grid">
+            <p>Likes</p>
+            <p>Views</p>
+            <p>Comment</p>
+            <p>Downloads</p>
+            <span>${image.likes}</span>
+            <span>${image.views}</span>
+            <span>${image.comments}</span>
+            <span>${image.downloads}</span>
+          </div>
+        </a>
+      </li>`
+    )
+    .join(' ');
+  removeLoadStroke(load);
+  box.insertAdjacentHTML('beforeend', markup);
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  lightbox.refresh();
 }
