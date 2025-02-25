23903396f370d3dcd59dd7400962d6a6d8411651
Artem Zakharchuk <zakharchukartem18@gmail.com>
2025-02-25 12:18:57 +0200

    New change!

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,32 @@
-
+import { markup } from '/js/render-functions';
+import { removeLoadStroke } from '/js/render-functions';
 import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import errorIcon from '../img/error.svg';
+
+
+const box = document.querySelector('.gallery');
+const load = document.querySelector('.load');
+const iziOption = {
+  messageColor: '#FAFAFB',
+  messageSize: '16px',
+  backgroundColor: '#EF4040',
+  iconUrl: errorIcon,
+  transitionIn: 'bounceInLeft',
+  position: 'topRight',
+  displayMode: 'replace',
+  closeOnClick: true,
+};
+
+
+let perPage = 15;
+let page = 1;
 
 
 export async function getImage(page) {
   const API_KEY = '48883219-a8d6c51151168ef356226c6a1';
+  const input = document.querySelector('.user-input');
   const query = encodeURIComponent(input);
   const urlParams = new URLSearchParams({
     key: API_KEY,
@@ -12,7 +35,7 @@ export async function getImage(page) {
     orientation: 'horizontal',
     safesearch: 'true',
     page: page,
-    per_page: perPage,
+    per_page: 40,
   });
 
   const URL = `https://pixabay.com/api/?${urlParams}`;
